faa2d11fc0b8f1e591db06e810cc3b064c3058fc
Adam Children <adam.children@themix.org.uk>
2023-09-06 23:41:17 +0100

    AdamC: Slight refactor on tests

--- tests/Feature/RateTest.php
+++ tests/Feature/RateTest.php
@@ -7,8 +7,30 @@ use App\Currency\Adapter;
 use Tests\TestCase;
 use Mockery;
 
-class RateTest extends TestCase {
+class RateTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private string $endpoint = '/api/rate';
 
+    /**
+     * @var User
+     */
+    private User $user;
+
+    /**
+     * @return void
+     */
+    private function setup(): void
+    {
+        parent::setup();
+        $this->user = User::factory()->make();
+    }
+
+    /**
+     * @return array[]
+     */
     public static function currencyConversionProvider(): array
     {
         return [
@@ -37,44 +59,41 @@ class RateTest extends TestCase {
     public function test_it_returns_the_converted_currency(string $base, string $currency, float $rate): void
     {
         $this->app->instance(Adapter::class, $this->createMockAdapter($rate, $base, $currency));
-        $user = User::factory()->make();
-        $this->actingAs($user, 'sanctum');
 
-        $response = $this->withHeaders(['X-Base-Currency' => $base])->get("/api/rate?currency={$currency}");
-
-        $response->assertStatus(200);
-        $response->assertJson(['data' => ['rate' => $rate]]);
+        $this
+            ->actingAs($this->user, 'sanctum')
+            ->withHeaders(['X-Base-Currency' => $base])
+            ->get("{$this->endpoint}?currency={$currency}")
+            ->assertStatus(200)
+            ->assertJson(['data' => ['rate' => $rate]]);
     }
 
     public function test_it_returns_404_for_unknown(): void
     {
-        $user = User::factory()->make();
-        $this->actingAs($user, 'sanctum');
-
-        $response = $this->withHeaders(['X-Base-Currency' => 'DOES_NOT_EXIST'])->get('/api/rate');
-
-        $response->assertStatus(404);
+        $this
+            ->actingAs($this->user, 'sanctum')
+            ->withHeaders(['X-Base-Currency' => 'DOES_NOT_EXIST'])
+            ->get($this->endpoint)
+            ->assertStatus(404);
     }
 
     public function test_it_returns_404_for_gbp_to_unknown(): void
     {
-        $user = User::factory()->make();
-        $this->actingAs($user, 'sanctum');
-
-        $response = $this->withHeaders(['X-Base-Currency' => 'DOES_NOT_EXIST'])->get('/api/rate');
-
-        $response->assertStatus(404);
+        $this
+            ->actingAs($this->user, 'sanctum')
+            ->withHeaders(['X-Base-Currency' => 'DOES_NOT_EXIST'])
+            ->get($this->endpoint)
+            ->assertStatus(404);
     }
 
     public function test_it_allows_an_override_when_passing_a_query_parameter(): void
     {
-        $user = User::factory()->make();
-        $this->actingAs($user, 'sanctum');
         $this->app->instance(Adapter::class, $this->createMockAdapter(1, 'GBP', 'GBP'));
 
-        $response = $this->withHeaders(['X-Base-Currency' => 'USD'])->get('/api/rate?base=GBP&currency=GBP');
-
-        $response->assertStatus(200);
-        $response->assertJson(['data' => ['rate' => 1]]);
+        $this
+            ->actingAs($this->user, 'sanctum')
+            ->withHeaders(['X-Base-Currency' => 'USD'])->get("{$this->endpoint}?base=GBP&currency=GBP")
+            ->assertStatus(200)
+            ->assertJson(['data' => ['rate' => 1]]);
     }
 }
