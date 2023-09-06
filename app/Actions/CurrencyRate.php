ac17f99bf0cb45eece0b0f61e29f9e18f3a0a779
Adam Children <adam.children@themix.org.uk>
2023-09-06 23:42:31 +0100

    AdamC: init CurrencyRate action and exception

---
+++ app/Actions/CurrencyRate.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Actions;
+
+use App\Exceptions\CurrencyRateConversionDoesNotExist;
+
+class CurrencyRate
+{
+    /**
+     * @var array|array[]
+     */
+    private array $rates = [
+        'EUR' => [
+            'EUR' => 1,
+            'GBP' => 0.85,
+            'USD' => 11.09
+        ],
+        'GBP' => [
+            'EUR' => 1.17,
+            'GBP' => 1,
+            'USD' => 1.28
+        ],
+        'USD' => [
+            'EUR' => 0.92,
+            'GBP' => 0.78,
+            'USD' => 1
+        ]
+    ];
+
+    /**
+     * @param string $base
+     * @param string $currencyCode
+     * @return float
+     * @throws CurrencyRateConversionDoesNotExist
+     */
+    public function convert(string $base, string $currencyCode): float
+    {
+        $this->isValidRate($base, $currencyCode);
+
+        return round($this->rates[$base][$currencyCode], 2);
+    }
+
+    /**
+     * @param string $base
+     * @param string $currencyCode
+     * @return void
+     * @throws CurrencyRateConversionDoesNotExist
+     */
+    private function isValidRate(string $base, string $currencyCode): void
+    {
+        if (!isset($this->rates[$base]) || !array_column($this->rates, $currencyCode)) {
+            throw new CurrencyRateConversionDoesNotExist();
+        }
+    }
+}
