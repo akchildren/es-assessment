d788fcad6d73e1f918849436918d089cd8ca125d
Adam Children <adam.children@themix.org.uk>
2023-09-06 23:41:51 +0100

    AdamC: create base api and controller route

---
+++ app/Http/Controllers/RateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+
+class RateController extends Controller
+{
+    /**
+     * TODO:: Validate that the request has valid parameters passed ALONG side headers
+     * Provision a new web server.
+     */
+    public function __invoke(Request $request)
+    {
+        // ..
+
+    }
+}
