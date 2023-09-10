49783f6ead2adf97bb9426e9f6bcbf36b60fdc9a
Adam Children <adam.children@themix.org.uk>
2023-09-10 19:56:39 +0100

    AdamC: improve consistency of adapter

--- app/Currency/Adapter.php
+++ app/Currency/Adapter.php
@@ -6,11 +6,16 @@ use App\Models\CurrencyRate;
 
 class Adapter implements Contract
 {
+    /**
+     * @param string $base
+     * @param string $currency
+     * @return float
+     */
     public function getRate(string $base, string $currency): float
     {
         return CurrencyRate::whereParentCurrencyCode($base)
             ->whereTargetCurrencyCode($currency)
             ->first()
-            ->pluck('rate');
+            ->rate;
     }
 }
