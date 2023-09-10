69dd8cd4a8efc8bbe1d05f7e60b28d11b64b103c
Adam Children <adam.children@themix.org.uk>
2023-09-10 13:29:10 +0100

    AdamC: create new CurrencyRateSeeder for currencies being used

---
+++ database/seeders/CurrencyRateSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CurrencyRate;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CurrencyRateSeeder extends Seeder
+{
+    /**
+     * @return void
+     */
+    public function run(): void
+    {
+        foreach (CurrencyRate::RATES as $parentCode => $rates) {
+            foreach ($rates as $targetCode => $rate) {
+                DB::table('currency_rates')->insert([
+                    'parent_currency_code' => $parentCode,
+                    'target_currency_code' => $targetCode,
+                    'rate' => $rate,
+                ]);
+            }
+        }
+    }
+}
